c03494828b605f552597917f9a62e5d5c4ee78dd
Mikomaks <ytmikos@gmail.com>
2025-05-19 09:23:38 +0200

    added script for running file

---
+++ IMDB/app.py
@@ -0,0 +1,29 @@
+from models.User import User
+from models.Movie import Movie
+from models.Review import Review
+from storage.File_Handler import File_Handler
+import streamlit as st
+import os
+
+#/mount/src/imdb/IMDB
+
+
+st.header(":orange[IM]:grey[DB]", divider="orange")
+st.header("Imperium mitów, dezinformacji i bredni", divider="grey")
+
+@st.cache_data
+def loadusers():
+    #File_Handler.loaduserfromfile("/mount/src/imdb/IMDB/users_saved")
+    File_Handler.loaduserfromfile("./users_saved")
+    return File_Handler.user_list
+
+
+users = loadusers()
+
+title = st.text_input("Username")
+if title:
+    user_var: User = next((user for user in users if user.username == title), None)
+    if user_var != None:
+        st.success(f"Pomyślnie znaleziono usera o ID:{user_var.id}")
+    else:
+        st.error(f"Brak użytkownika {title} w bazie danych!")
--- IMDB/main.py
+++ IMDB/main.py
@@ -1,30 +1,5 @@
-import time
-
-from models.User import User
-from models.Movie import Movie
-from models.Review import Review
-from storage.File_Handler import File_Handler
-import streamlit as st
 import os
 
-#/mount/src/imdb/IMDB
+if __name__ == "__main__":
+    os.system("streamlit run app.py")
 
-st.header(":orange[IM]:grey[DB]", divider="orange")
-st.header("Imperium mitów, dezinformacji i bredni", divider="grey")
-
-
-@st.cache_data
-def loadusers():
-    File_Handler.loaduserfromfile("/mount/src/imdb/IMDB/users_saved")
-    return File_Handler.user_list
-
-
-users = loadusers()
-
-title = st.text_input("Username")
-if title:
-    user_var: User = next((user for user in users if user.username == title), None)
-    if user_var != None:
-        st.success(f"Pomyślnie znaleziono usera o ID:{user_var.id}")
-    else:
-        st.error(f"Brak użytkownika {title} w bazie danych!")
