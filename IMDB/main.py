5f092c00b744038f8e22cc4e8ce0ad7f34648158
Mikomaks <ytmikos@gmail.com>
2025-05-19 08:41:02 +0200

    god bless morning coffee

--- IMDB/main.py
+++ IMDB/main.py
@@ -6,26 +6,13 @@ from models.Review import Review
 from storage.File_Handler import File_Handler
 import streamlit as st
 
-@st.cache_resource
-def fetch_data(file_name:str):
-    result = list()
-    file = open(file_name,'r')
-    for line in file:
-        line = line.strip() #czyscimy ze new line
-        elems = line.split(sep=':')
-        #elems ['101', 'maciekKox', 'hash']
-        user = User()
-        user.overrideID(int(elems[0]))
-        user.setusername(elems[1].lower())
-        user.setpasswordNoHash(elems[2])
-        File_Handler.user_list.append(user)
-        result.append(user)
-    return result
 
 
 
 
-data = fetch_data("./users_saved")
+
+
+data = File_Handler.loaduserfromfile("../IMDB/users_saved")
 
 for user in data:
     st.text(user)
