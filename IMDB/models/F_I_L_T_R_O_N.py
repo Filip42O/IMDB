8bef4e8ee9f461ed08ae9dee80af3fd1ec3461ce
s30819 <s30819@pjwstk.edu.pl>
2025-05-20 16:00:41 +0200

    filtron update

--- IMDB/models/F_I_L_T_R_O_N.py
+++ IMDB/models/F_I_L_T_R_O_N.py
@@ -1,25 +1,54 @@
 from re import search
 from typing import List
 
+from models.Category import Category
 from models.Movie import Movie
 
 
 class F_I_L_T_R_O_N:
 
-
-    def __init__(self, movies : List[Movie]):
+    def __init__(self, movies: List[Movie]):
         self.movies = movies
         self.movies_dict = dict()
+        self.result = list()
 
         for movie in movies:
             self.movies_dict[frozenset(movie.Title.lower().split())] = movie
 
-
-
-    def search(self, word : str) -> List[Movie]:
+    def search(self, word: str):
         search_list = set(word.lower().split(sep=' '))
         result_list = list()
         for titleSet, movie in self.movies_dict.items():
             if search_list.issubset(titleSet):
                 result_list.append(movie)
         return result_list
+
+    def filter_by_category(self, categories_as_string: str):
+        categories_splited = categories_as_string.split(sep=',')
+        categories = list()
+
+        for category in categories_splited:
+            categories.append(Category[category])
+
+        for cat in categories:
+            for mov in self.movies:
+                if cat in mov.Categories:
+                    self.result.append(mov)
+
+    def filter_by_director(self, directors_as_string: str):
+        directors_splited = directors_as_string.split(sep=',')
+
+        for dire in directors_splited:
+            for mov in self.movies:
+                if dire == mov.Director:
+                    self.result.append(mov)
+
+    def filter_by_length(self, minLength: int, maxLength: int):
+        for mov in self.movies:
+            if minLength <= mov.Length <= maxLength:
+                self.result.append(mov)
+
+    def filter_by_year(self, minYear: int, maxYear: int):
+        for mov in self.movies:
+            if minYear <= mov.Year <= maxYear:
+                self.result.append(mov)
--- IMDB/tests/F_I_L_T_R_O_N_TESTS.py
+++ IMDB/tests/F_I_L_T_R_O_N_TESTS.py
@@ -7,7 +7,7 @@ movie1 = Movie("The Shawshank Redemption", 142, 1994, "Frank Darabont")
 movie1.addcategory(Category.Drama)
 
 movie2 = Movie("Pulp Fiction", 154, 1994, "Quentin Tarantino")
-movie2.addcategory(Category.Drama)
+movie2.addcategory(Category.Action)
 
 movie3 = Movie("Schindler's List", 195, 1993, "Steven Spielberg")
 movie3.addcategory(Category.War)
@@ -17,10 +17,20 @@ movie4.addcategory(Category.Science_Fiction)
 movie4.addcategory(Category.Drama)
 movie4.addcategory(Category.Romance)
 
-
 movies = [movie1, movie2, movie3, movie4]
 
 filtron = F_I_L_T_R_O_N(movies)
 
-for res in filtron.search("THE"):
-    print(res)
+filtron.filter_by_year(1994, 2015)
+
+# filtron.filter_by_length(150, 170)
+
+# filtron.filter_by_director("Quentin Tarantino,Frank Darabont")
+
+# filtron.filter_by_category("Drama,Action")
+#
+# for res in filtron.search("THE"):
+#     print(res)
+
+for result in filtron.result:
+    print(result)
